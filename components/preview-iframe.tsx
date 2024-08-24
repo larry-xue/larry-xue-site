f2fc4f1c51de28181ca9e8afed3b3d5a0f6cb053
larry-xue <azoux0311@gmail.com>
2024-08-24 23:05:18 +0800

    fix: add title to iframe and theme switcher, lazy loading iframe

--- components/preview-iframe.tsx
+++ components/preview-iframe.tsx
@@ -1,3 +1,3 @@
 export default function PreviewIframe({ url }: { url: string }) {
-  return <iframe src={url} className="w-full h-full" />;
+  return <iframe title="preview" loading="lazy" src={url} className="w-full h-full" />;
 }
