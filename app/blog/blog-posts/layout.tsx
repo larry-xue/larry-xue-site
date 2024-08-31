25226525f383c470c40c9ad359e80a037db0e988
larry-xue <azoux0311@gmail.com>
2024-08-31 09:37:45 +0800

    convert nextjs to astro

--- app/blog/blog-posts/layout.tsx
+++
@@ -1,6 +0,0 @@
-import "./blog.css";
-
-export default function MdxLayout({ children }: { children: React.ReactNode }) {
-  // Create any shared layout or styles here
-  return <div className="my-12 w-full mdx-container">{children}</div>
-}
--- components/preview-iframe.tsx
+++
@@ -1,3 +0,0 @@
-export default function PreviewIframe({ url }: { url: string }) {
-  return <iframe title="preview" loading="lazy" src={url} className="w-full h-full" />;
-}
